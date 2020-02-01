e9c96b8fd7218f9ae1794a8a198e8cf8c749347d
JM Software <jmsoftware@MacBook-Air-de-JM.local>
2020-02-01 01:04:41 -0300

    inicio do desenvolvimento

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Olá, alô.</Text>
     </View>
   );
 }
@@ -12,8 +12,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#7159c1",
+    alignItems: "center",
+    justifyContent: "center"
   },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 32,
+    color: "#fff"
+  }
 });
